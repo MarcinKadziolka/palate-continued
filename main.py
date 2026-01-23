a535bc901c8ccdc28bf1a8a815342018adaeeea5
Tadeusz Dziarmaga <tadeuszdziarmaga@MacBook-Air-Tadeusz.local>
2026-01-23 15:03:21 +0100

    not skipping empty pierwszy skladnik

--- main.py
+++ main.py
@@ -659,9 +659,6 @@ def main():
         gen_gt = gen_representations[mask_keep]
         gen_lt = gen_representations[mask_low]
 
-        if len(gen_gt) == 0 or len(gen_lt) == 0:
-            logger.warning(f"Skipping {gen_path}: empty gen_gt or gen_lt")
-            continue
 
         f_gt = len(gen_gt) / len(gen_representations)
         f_lt = len(gen_lt) / len(gen_representations)
