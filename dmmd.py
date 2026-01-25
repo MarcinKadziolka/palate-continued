32c34294b5a4d363cc456b61aa10c431a56b8e1a
Tadeusz Dziarmaga <tadeuszdziarmaga@MacBook-Air-Tadeusz.local>
2026-01-25 18:33:29 +0100

    bug fix

--- dmmd.py
+++ dmmd.py
@@ -222,8 +222,8 @@ def blockwise_kernel_mean_old(x, y, sigma):
 @jax.jit
 def dmmd_blockwise(x: np.ndarray, y: np.ndarray, sigma: float) -> tuple[jax.Array, jax.Array]:
     """Computes D-MMD using blockwise kernel computation."""
-    mean_kxx = blockwise_kernel_mean(x, x, sigma)
-    mean_kxy = blockwise_kernel_mean(x, y, sigma)
-    mean_kyy = blockwise_kernel_mean(y, y, sigma)
+    mean_kxx = kernel_mean(x, x, sigma, block_size=1000)
+    mean_kxy = kernel_mean(x, y, sigma, block_size=1000)
+    mean_kyy = kernel_mean(y, y, sigma, block_size=1000)
 
     return mean_kxx + mean_kyy - 2 * mean_kxy, mean_kxx + mean_kyy
