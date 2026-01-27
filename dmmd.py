a2224673059ffad77aa862e8155dc26a1536d15b
Tadeusz Dziarmaga <tadeuszdziarmaga@MacBook-Air-Tadeusz.local>
2026-01-27 01:33:49 +0100

    speed

--- dmmd.py
+++ dmmd.py
@@ -4,11 +4,7 @@ from jax import lax
 
 
 @jax.jit
-def _rbf_block_precomputed(x, y, x2, y2, sigma):
-    # x: [B1, D]
-    # y: [B2, D]
-    # x2: [B1]
-    # y2: [B2]
+def _rbf_block(x, y, x2, y2, sigma):
     return jnp.exp(
         -(x2[:, None] + y2[None, :] - 2.0 * (x @ y.T))
         / (2.0 * sigma**2)
@@ -16,7 +12,8 @@ def _rbf_block_precomputed(x, y, x2, y2, sigma):
 
 
 def kernel_mean_blockwise(x, y, sigma, block_size=1024):
-    n, m = x.shape[0], y.shape[0]
+    n, d = x.shape
+    m = y.shape[0]
 
     x2 = jnp.sum(x * x, axis=1)
     y2 = jnp.sum(y * y, axis=1)
@@ -24,16 +21,17 @@ def kernel_mean_blockwise(x, y, sigma, block_size=1024):
     def outer_loop(i, acc):
         total, count = acc
 
-        xb = x[i:i + block_size]
-        x2b = x2[i:i + block_size]
+        xb = lax.dynamic_slice(x, (i, 0), (block_size, d))
+        x2b = lax.dynamic_slice(x2, (i,), (block_size,))
 
         def inner_loop(j, inner_acc):
             total_inner, count_inner = inner_acc
 
-            yb = y[j:j + block_size]
-            y2b = y2[j:j + block_size]
+            yb = lax.dynamic_slice(y, (j, 0), (block_size, d))
+            y2b = lax.dynamic_slice(y2, (j,), (block_size,))
 
-            k = _rbf_block_precomputed(xb, yb, x2b, y2b, sigma)
+            k = _rbf_block(xb, yb, x2b, y2b, sigma)
+
             return (
                 total_inner + jnp.sum(k),
                 count_inner + k.size,
