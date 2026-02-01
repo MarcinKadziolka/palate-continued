0e7e9646c7bbf9d6935e7aad677e9cf8556ab5de
Tadeusz Dziarmaga <tadeuszdziarmaga@MacBook-Air-Tadeusz.local>
2026-02-01 02:47:12 +0100

    tests

--- palate.py
+++ palate.py
@@ -1,7 +1,7 @@
 import time
 import logging
 import numpy as np
-from dmmd import dmmd_blockwise_jax
+from dmmd import dmmd_blockwise_jax, compute_all_dmmd
 import jax
 import jax.numpy as jnp
 from jax import lax
