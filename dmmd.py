ee2aa15f4724152490b0f06bb2806443721b0063
Tadeusz Dziarmaga <tadeuszdziarmaga@MacBook-Air-Tadeusz.local>
2026-02-01 02:35:07 +0100

    tests

--- dmmd.py
+++ dmmd.py
@@ -34,8 +34,41 @@ def kernel_mean_blockwise(x, y, sigma, block_size=1024):
 # ------------------------------------------------------------
 # MMD
 # ------------------------------------------------------------
-def dmmd_blockwise_jax(x, y, sigma, block_size=1024):
+def dmmd_blockwise_jax(x, y, sigma, block_size=512):
     kxx = kernel_mean_blockwise(x, x, sigma, block_size)
     kyy = kernel_mean_blockwise(y, y, sigma, block_size)
     kxy = kernel_mean_blockwise(x, y, sigma, block_size)
     return kxx + kyy - 2.0 * kxy, kxx + kyy
+
+import jax
+import jax.numpy as jnp
+from functools import partial
+
+@partial(jax.jit, static_argnames=("sigma",))
+def compute_all_dmmd(
+    train,
+    test,
+    gen,
+    gt,
+    sigma,
+):
+    sigma3 = sigma / 3
+
+    d_train_gen, _ = dmmd_blockwise_jax(train, gen, sigma)
+    d_test_gen, denom = dmmd_blockwise_jax(test, gen, sigma)
+
+    d_train_gt, _ = dmmd_blockwise_jax(train, gt, sigma3)
+    d_test_gt, _ = dmmd_blockwise_jax(test, gt, sigma3)
+
+    palate = d_test_gt / (d_test_gt + d_train_gt)
+    m_palate = d_test_gen / (2 * denom) + 0.5 * palate
+
+    return (
+        palate,
+        m_palate,
+        d_train_gen,
+        d_test_gen,
+        d_train_gt,
+        d_test_gt,
+        denom,
+    )
