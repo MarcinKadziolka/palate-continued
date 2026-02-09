601b2e7128b57a444dda18535ba5b092abacb87b
Marcin <kadziolka.marcin@outlook.com>
2026-02-09 05:23:58 +0100

    Remove docstring

--- models/dinov3.py
+++ models/dinov3.py
@@ -46,8 +46,6 @@ class DINOv3Encoder(Encoder):
         self.model.eval()
 
     def transform(self, img):
-        """Transformacja obrazu do formatu wejściowego DINOv3"""
-        # normalizacja taka jak dla ImageNet
         imagenet_mean = np.array([0.485, 0.456, 0.406])
         imagenet_std = np.array([0.229, 0.224, 0.225])
 
